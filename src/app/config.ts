4d01e1ca82d182154f2ee733677023a0e112a18d
brayan salazar <brayan.salazar25@gmail.com>
2019-08-10 10:58:35 -0500

    se configura conexion api

---
+++ src/app/config.ts
@@ -0,0 +1,6 @@
+export const ConfigApi = {
+    urlBase: 'http://gateway.marvel.com/',
+    privateKey: 'b4d9723cb8a2561c1a7b7d9e95a761074ad12a8b',
+    publicKey: 'bc68dd5e9b4fc308b2a173c18f660677'
+};
+
